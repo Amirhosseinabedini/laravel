232bdcf7ff4478f74c1c94169c0ad3eaf16ea537
Amirabedini <abedinia313@gmail.com>
2023-01-31 02:32:20 +0330

    basics update

--- routes/web.php
+++ routes/web.php
@@ -16,34 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-route::get('/contact', function (){
-    return "خوش امدید";
-});
-
-route::get('/about', function (){
-    return "درباره ما";
-});
-
-route::get('post/{id?}/{name?}', function ($id= null ,$name= null){
-return"name in post = ".$name ." and id = ". $id;
-});
-route::get('/admin/post/example', function (){
-    $url= route('admin');
-    return"این صفحه برای ادمین میباشد و url برابر با". $url;
-})->name('admin');
-
-route::get('/admin/login' , function (){
-    return  'صفحه ورود مدیریت';
-});
-
-//route::redirect('admin/login','/roxo/laravel/laravel/public/admin/post/example',301);
-
-
-route::prefix('admin')->group(function (){
-    route::get('post/example',function (){
-        return'گروه بندی ادمین';
-    });    route::get('login',function (){
-        return'لاگین ادمین';
-    });
-});
